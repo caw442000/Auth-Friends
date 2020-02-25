2df296de6242754b51cd9cc8c53e54b004a4e026
cedric-winbush <cawinbushjr@gmail.com>
2020-02-25 15:12:51 -0600

    set friends list

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,7 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const FriendsList = props => {
-  const [friends, setFriends ] = useState([])
+  const [friends, setFriends ] = useState([]);
+
+  useEffect(() => {
+    const token = window.localStorage.getItem('token');
+    axiosWithAuth()
+    .get('/api/friends')
+    .then(res => {
+      console.log("this is the response for getting friends", res)
+      setFriends(res.data)
+    })
+  },[]);
+    
+  
   return (
     <div>
       <p>Friends List </p>
